2f234f4805b0b3214c09b5db8f9b5b122ce72642
nickniuchina <nick.niu.china@gmail.com>
2019-12-29 19:47:05 +0800

    update the git.cmd file!

--- git.cmd
+++ git.cmd
@@ -4,3 +4,17 @@ git commit -m "first commit" 
 git remote rm origin
 git remote add origin https://自己的仓库url地址
 git push -u origin develop
+
+
+
+
+
+
+
+echo "# mydj" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/NickNiuChina/mydj.git
+git push -u origin master
+
